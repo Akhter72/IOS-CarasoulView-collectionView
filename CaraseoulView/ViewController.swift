a4059bd5e0cd3e3f03b378f5e0b8dc28bb76365c
Akhter Hussain <akhterhussain.dar@ideas2it.com>
2023-07-30 17:11:26 +0530

    updated animation style

--- CaraseoulView/ViewController.swift
+++ CaraseoulView/ViewController.swift
@@ -8,81 +8,100 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    /*
+     1. collectionViewHeight = x // done
+     2. //189    --     layout.minimumLineSpacing = -189 // done
+     3. //x - 40 --     collectionView.frame.size.height += 40  // done
+     4. applyEffectToCurrentCell(offset: -200) (cleanup) // done
+     5. myItems.append(myItems[0]) myItems.remove(at: 0) - add check prevent crash // done
+     6. CGFloat(3 - indexPath.row) // done
+     7. return CGSize(width: collectionView.frame.width - 20 - wid , height: collectionView.frame.height - 45 ) // done
+     8. self.collectionView.reloadData() only if last index  // done
+     9.
+     
+     */
+    
+    
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var cellHeight = CGFloat(163)
+    var collectionViewHeight = CGFloat(193)
+    
+    
+    
     var myItems = [
         ["type": "Primary", "title": "WHAT TO DO NEXT", "subTitle": "Login to any website & save to your node.You can then swipe to sign in.", "Picture": "Search", "btnText": "Open Safari To Try This"],
         ["type": "Primary", "title": "WHAT TO DO NEXT", "subTitle": "Import your passwords from password manager", "btnText": "Import Passwords"],
         ["type": "Secondary", "title": "WHAT TO DO NEXT", "subTitle": "Make a purchase using your tokenized email, track from here p11williams@iomd.info", "Picture": "ReadLater", "btnText": "button Text 3"],
     ]
-    var firstCellDisplayed = false
-    var currentCellIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.isScrollEnabled = false
         if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             layout.scrollDirection = .vertical
-            layout.minimumLineSpacing = -189
+            // 5 value for ovelaying view
+            layout.minimumLineSpacing = -(cellHeight - 5)
             layout.minimumInteritemSpacing = 0
         }
     }
     
     override func viewDidLayoutSubviews() {
-        collectionView.frame.size.height += 40
+        collectionView.frame.size.height = collectionViewHeight
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 20, right: 10)
     }
     
-    func reloadDataWithFadeIn() {
-        collectionView.reloadData()
-        CATransaction.begin()
-        CATransaction.setAnimationDuration(0.2)
-        let transition = CATransition()
-        CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear))
-        transition.type = CATransitionType.fade
-//        transition.subtype = CATransitionSubtype.fromTop
-        collectionView.layer.add(transition, forKey: nil)
-        CATransaction.commit()
-    }
     func scrollToNextCell() {
-        myItems.append(myItems[0])
-        myItems.remove(at: 0)
-        applyEffectToCurrentCell(offset: -200)
+        if myItems.count >= 0 {
+            myItems.append(myItems[0])
+            myItems.remove(at: 0)
+        }
+        applyEffectToCurrentCell()
     }
     
-    func applyEffectToCurrentCell(offset: CGFloat) {
+    func applyEffectToCurrentCell() {
+        let cells = collectionView.visibleCells
         let indexPath = IndexPath(item: 0, section: 0)
-        if let cell = collectionView.cellForItem(at: indexPath) as? MyCell {
-            UIView.animate(withDuration: 0.6, delay: 0, options: .curveEaseInOut, animations: {
-                cell.transform = CGAffineTransform(translationX: 0, y: offset)
+        for cell in cells {
+            var off = -(cell.frame.height/2)
+            if indexPath == collectionView.indexPath(for: cell) {
+                UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+                    cell.transform = CGAffineTransform(translationX: 0, y: off - 30).scaledBy(x: 0.95, y: 0.8)
+                }, completion: {_ in
+                    cell.layer.zPosition = -1
+                    UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+                        off = -(off/4)
+                        cell.transform = CGAffineTransform(translationX: 0, y: off).scaledBy(x: 0.95, y: 0.6)
+                    
+                    })
+                })
+            } else {
+                var myCount = 0
+                var off2 = cell.frame.height / 4
+                UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+                    cell.transform = CGAffineTransform(translationX: 0, y: off2).scaledBy(x: 1, y: 1)
                 
-            }, completion: {_ in
-                self.reloadDataWithFadeIn()
-
-            })
-        }
-    }
-    
-    func applyMovingEffectToCurrentCell(with offsetY: CGFloat) {
-        let indexPath = IndexPath(item: currentCellIndex, section: 0)
-        if let cell = collectionView.cellForItem(at: indexPath) as? MyCell {
-            cell.transform = CGAffineTransform(translationX: 0, y: offsetY)
-        }
-    }
-    
-    func resetEffectOnCurrentCell() {
-        let indexPath = IndexPath(item: currentCellIndex, section: 0)
-        if let cell = collectionView.cellForItem(at: indexPath) as? MyCell {
-            UIView.animate(withDuration: 0.3) {
-                cell.transform = CGAffineTransform(translationX: 10, y: 20)
+                }, completion: {_ in
+                    UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+                        off2 = -50
+                        cell.transform = CGAffineTransform(translationX: 0, y: -5).scaledBy(x: 1.06 , y: 1)
+                        
+                    }, completion: { [self]_ in
+                        if self.collectionView.indexPath(for: cell)?.row == cells.count - 1 {
+                            self.collectionView.reloadData()
+//                            self.collectionView.reloadItems(at: [IndexPath(row: cells.count, section: 0)])
+                        }
+                    })
+                })
+                myCount += 1
             }
         }
     }
-    
-    func scrollToPreviousCell() {
-        myItems.insert(myItems[myItems.count - 1], at: 0)
-        myItems.remove(at: myItems.count - 1)
-        applyEffectToCurrentCell(offset: 200)
-    }
-    
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -92,7 +111,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCell
-        cell.layer.zPosition = CGFloat(3 - indexPath.row)
+        cell.layer.zPosition = CGFloat(myItems.count - indexPath.row)
         cell.layer.cornerRadius = 12
         cell.layer.shadowColor = UIColor.gray.cgColor
         cell.layer.shadowOffset = CGSize(width: 0, height: 2)
@@ -120,13 +139,14 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
             cell.cellBtn.isHidden = true
             cell.secondaryView.isHidden = false
         }
-        if indexPath.row == 0 {
-            cell.backgroundColor = UIColor(hex: 0xE1ECE9)
-            cell.layer.borderWidth = 0
-        } else {
-            cell.backgroundColor = UIColor(hex: 0xCFE8E1)
-            cell.layer.borderWidth = 1
-        }
+        cell.backgroundColor = UIColor(hex: 0xE1ECE9)
+//        if indexPath.row == 0 {
+//            cell.backgroundColor = UIColor(hex: 0xE1ECE9)
+//            cell.layer.borderWidth = 0
+//        } else {
+//            cell.backgroundColor = UIColor(hex: 0xCFE8E1)
+//            cell.layer.borderWidth = 1
+//        }
         
         cell.layer.borderColor = UIColor(hex: 0xC1DDD5).cgColor
         let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
@@ -144,8 +164,8 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let wid = CGFloat(indexPath.row * 20)
-        return CGSize(width: collectionView.frame.width - 20 - wid , height: collectionView.frame.height - 45 )
+        let wid = CGFloat(indexPath.row * 20) + 20
+        return CGSize(width: collectionView.frame.width - wid , height: cellHeight )
     }
 }
 
@@ -157,4 +177,3 @@ extension UIColor {
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
-
